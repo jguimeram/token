0912215d8ae97538b5102a23ed0119768d8f93e8
jguimeram <jguimera54@gmail.com>
2025-09-03 16:12:52 +0200

    array of tokens with malloc

--- main.c
+++ main.c
@@ -20,6 +20,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#define MAX_CAPACITY 20
+
 typedef enum{
   TOK_NUM,
   TOK_ADD,
@@ -45,32 +47,46 @@ int main(int argc, char **argv){
   
     const char *p = argv[1];
     char *end;
-   
+     
+    Token *exp = (Token *)malloc(sizeof(Token)*MAX_CAPACITY);
+
+    /* 
+    printf("%ld\n", sizeof(*exp)); //print the size of one token + padding
     printf("size of p: %ld\n", sizeof(p)); //8 bytes:
     printf("size of *p: %ld\n", sizeof(*p)); //1 byte (char)
+*/
+    int i = 0;
 
     while(*p){
-      if(isdigit(*p)){
-        Token t;
-        t.value = strtol(p, &end, 10);  
-        t.type = TOK_NUM;
-        printf("partial sum: %ld\n", t.value);
-        printf("type: %d\n", t.type);
-        p = end;
-      }else if(*p == '+'){
-        Token t;
-        t.value = 0;
-        t.type = TOK_ADD;
-      }else if(*p == '-'){
-        Token t;
-        t.value = 0;
-        t.type = TOK_SUB;
-      }
-      p++;
+          //   printf("index %d\n", i); why runs 15 times?
+             if(isdigit(*p)){
+                Token t;
+                t.value = strtol(p, &end, 10);  
+                t.type = TOK_NUM;
+                //printf("partial sum: %ld\n", t.value);
+                //printf("type: %d\n", t.type);
+                exp[i] = t;
+                p = end;
+              }else if(*p == '+'){
+                Token t;
+                t.value = 0;
+                t.type = TOK_ADD;
+                exp[i] = t;
+              }else if(*p == '-'){
+                Token t;
+                t.value = 0;
+                t.type = TOK_SUB;
+                exp[i] = t;
+             }
+       p++;
+       i++;
     }
-  
-  //printf("sum: %ld\n", sum);
 
-    
+   printf("Printing array of struct...:\n"); 
+   for(int i = 0; i < MAX_CAPACITY; i++){
+
+    printf("%d %ld\n", exp[i].type, exp[i].value);
+   }
+
     return 0;
 }
